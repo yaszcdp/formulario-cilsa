89de18cdeaac3eac37a4de7237acc09b9095db34
yaszcdp <yasz.utn@hotmail.com>
2024-07-16 17:53:26 -0300

    pequeños detalles y devolviendo el js original

--- formulario.js
+++ formulario.js
@@ -19,7 +19,6 @@ function submitForm(){
             }else{
                 toggleError(field, false);
             }
-
             data[field] = fieldValue;
         });
 
@@ -44,17 +43,12 @@ function addBlurEventListeners(){
     const fields = ['nombre', 'apellido', 'email', 'fecha_nac', 'pais'];
 
     fields.forEach(field =>{
-        const element = document.getElementById(field);
-        if (element) {
-            element.addEventListener('blur', function(){
-                const isInvalid = this.value === '';
-                toggleError(field, isInvalid);
-            });
-        } else {
-            console.error(`Elemento no encontrado: ${field}`);
-        }
+        document.getElementById(field).addEventListener('blur', function(){
+            const isInvalid = this.value === '';
+            toggleError(field, isInvalid);
+        });
     });
-} 
+}
 
 //Función para obtener la fecha actual y poder setearla como fecha máxima en el campo de fecha de nacimiento
 function maxDate(){
@@ -67,12 +61,6 @@ function maxDate(){
     return fechaMaxima;
 } 
 
-//Funcion para validación de email
-function isValidEmail(email){
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,4}\.[0-9]{1,4}\.[0-9]{1,4}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
-
 //Funciones para cambiar de estilo. 
 function setHighContrastStyle(){
     document.body.classList.add('high-contrast');
@@ -81,5 +69,3 @@ function setHighContrastStyle(){
 function setClassicStyle(){
     document.body.classList.remove('high-contrast');
 }
-
-
